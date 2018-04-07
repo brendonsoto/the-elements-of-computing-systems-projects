c321c4af713d316ad30eb6ae6c2657647a9bf900
brendon <bsoto@nhl.com>
2018-04-06 20:49:39 -0400

    Completed the tokenizer module for project 10

---
+++ projects/compiler/tokenizer.py
@@ -0,0 +1,102 @@
+keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
+symbols = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+
+
+def tokenize(jack_file):
+    tokens = get_tokens(jack_file)
+    tokens_as_xml = map(get_xml, tokens)
+    output_file = jack_file.replace('.jack', '_tokenized.xml')
+    write_output(output_file, tokens_as_xml)
+
+
+def get_tokens(jack_file):
+    tokens = []
+    with open(jack_file, 'r') as f:
+        for line in f:
+            if (not line.isspace() and
+                    not line.startswith('//') and
+                    not line.startswith('/*')):
+                code_without_comments = remove_comments(line).strip()
+                tokens.extend(get_symbols(code_without_comments))
+    return tokens
+
+
+def remove_comments(line_of_code):
+    if '//' in line_of_code:
+        return line_of_code[0:line_of_code.index('//') - 1]
+    if '/*' in line_of_code:
+        return line_of_code[0:line_of_code.index('/*') - 1]
+    return line_of_code
+
+
+'''
+get_symbols( string )
+returns an array of strings
+
+Iterates through the characters of a line of code to get the tokens associated with the Jack language
+'''
+def get_symbols(line_of_code):
+    symbols_from_code = []
+    symbol_parts = [] # to be used per symbol/word
+    isCharPartOfString = False
+    for char in line_of_code:
+        if char == '"' or char == "'":
+            if not isCharPartOfString:
+                isCharPartOfString = True
+            else:
+                isCharPartOfString = False
+                symbol = "(str){0}".format(''.join(symbol_parts)) # Add a way to identify the token as a string
+                symbols_from_code.append(symbol)
+                symbol_parts.clear()
+        elif char.isspace() and len(symbol_parts) > 0 and not isCharPartOfString:
+            symbol = ''.join(symbol_parts)
+            symbols_from_code.append(symbol)
+            symbol_parts.clear()
+        elif char in symbols and not isCharPartOfString:
+            if len(symbol_parts) > 0:
+                symbol = ''.join(symbol_parts)
+                symbols_from_code.append(symbol)
+                symbol_parts.clear()
+            symbols_from_code.append(char)
+        elif not char.isspace() or isCharPartOfString:
+            symbol_parts.append(char)
+    return symbols_from_code
+
+
+def get_symbol_code(symbol):
+    code = symbol
+    if symbol == '<':
+        code = '&lt;'
+    elif symbol == '>':
+        code = '&gt;'
+    return code
+
+
+def get_xml(token):
+    xml = ''
+    if token in keywords:
+        xml = "<keyword> {0} </keyword>".format(token)
+    elif token in symbols:
+        xml = "<symbol> {0} </symbol>".format(get_symbol_code(token))
+    elif "(str)" in token:
+        xml = "<stringConstant> {0} </stringConstant>".format(token[5:])
+    elif token.isdigit():
+        xml = "<integerConstant> {0} </integerConstant>".format(token)
+    else:
+        xml = "<identifier> {0} </identifier>".format(token)
+    return xml
+
+
+def write_output(output_file, tokens_as_xml):
+    with open(output_file, 'w') as f:
+        f.write("<tokens>\n")
+        for token in tokens_as_xml:
+            f.write(token)
+            f.write("\n")
+        f.write('</tokens>')
+    print("All done writing XML to {0}".format(output_file))
+
+
+if __name__ == '__main__':
+    import sys
+    tokenize(sys.argv[1])
